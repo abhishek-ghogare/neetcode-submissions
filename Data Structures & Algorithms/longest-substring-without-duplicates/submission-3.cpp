00312ac460384b825c02835f9b375c02914a0c14
Abhishek Ghogare <7853048+abhishek-ghogare@users.noreply.github.com>
2026-07-23 22:10:45 +0530

    Add: longest-substring-without-duplicates - submission-3

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-3.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        vector<bool> visited(128, false);
+        int longestUniqueStrLength = 0;
+        int start = 0;
+
+        for (int i=0 ; i<s.size() ; ++i) {
+            if (visited[s[i]]) {
+                while (s[start] != s[i]) {
+                    visited[s[start]] = false;
+                    start++;
+                }
+                start++;
+            } else {
+                visited[s[i]] = true;
+                longestUniqueStrLength = max(longestUniqueStrLength, i-start+1);
+            }
+        }
+
+        return longestUniqueStrLength;
+    }
+};
